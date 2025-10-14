af943ef88fa915b3eeb403b0cd16cd88e4acb37a
kurizma <foriamkuri@gmail.com>
2025-10-14 09:03:26 +0300

    save before rebuild

--- src/main/java/com/jkim/lets_play/auth/JwtUtil.java
+++ src/main/java/com/jkim/lets_play/auth/JwtUtil.java
@@ -4,4 +4,67 @@ package com.jkim.lets_play.auth;
 // token validation
 // token parsing
 
-import io.jsonwebtoken
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    
+    private final Key SECRET_KEY = Keys.hmacShaKeyFor("your_256_bit_secret_key__must_be_32byteslong".getBytes());
+    private final long EXPIRATION = 86400000; // 1 day
+    // private final long EXPIRATION = 600000 // 10 min
+    // private final long EXPIRATION = 300000 // 5 minutes
+    // private final long EXPIRATION = 60000 // 1 minute
+    
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SECRET_KEY) // new pattern for algo? What?
+                .compact();
+    }
+    
+    public  boolean validateToken(String token) {
+
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(SECRET_KEY)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+        
+    }
+    
+    public String extractEmail(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getSubject();
+    }
+    
+    public String extractRole(String token) {
+        
+        Claims claims = Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.get("role", String.class);
+        
+    }
+    
+}
+
