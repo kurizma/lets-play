bb81f9c9a2c677be8a317055678610acc7accf85
kurizma <foriamkuri@gmail.com>
2025-10-21 11:49:38 +0300

    Adding Exceptions and GlobalHandler

--- src/main/java/com/jkim/lets_play/exception/AuthException.java
+++ src/main/java/com/jkim/lets_play/exception/AuthException.java
@@ -1,7 +1,21 @@
 package com.jkim.lets_play.exception;
 
 public class AuthException extends RuntimeException {
+    
+    public AuthException() {
+        super();
+    }
+    
     public AuthException(String message) {
         super(message);
     }
+    
+    public AuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public AuthException(Throwable cause) {
+        super(cause);
+    }
+    
 }
--- src/main/java/com/jkim/lets_play/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jkim/lets_play/exception/GlobalExceptionHandler.java
@@ -5,20 +5,28 @@ import org.springframework.http.*;
 import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidation(MethodArgumentNotValidException ex) {
-        String error = ex.getBindingResult().getFieldErrors().stream().map(f ->f.getField() + ": " + f.getDefaultMessage())
-                .findFirst().orElse("Invalid request data");
+        String error = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(err ->err.getField() + ": " + err.getDefaultMessage())
+                .findFirst()
+                .orElse("Invalid request data");
         return ResponseEntity.badRequest().body(error);
     }
+    // 400
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    
     
     @ExceptionHandler(AuthException.class)
-    public ResponseEntity<String> handleAuth(AuthException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    public ResponseEntity<?> handleAuth(AuthException ex) {
+        return buildResponse(HttpStatus.UNAUTHORIZED, ex.getMessage());
     }
+
     
 }
